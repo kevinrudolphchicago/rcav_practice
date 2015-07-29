94854f280d486e8db093036ba9f5b44c1b0e48af
kevinrudolphchicago <kevinrudolph571@gmail.com>
2015-07-28 23:25:20 -0700

    Rudolph_rcav_practice

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,59 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+     @user_move = params["move"]
+     @tova = @user_move.to_i
+     @noah = (@tova)**2
+  end
+
+  def square_root
+     @user_move = params["move"]
+     @kevin = @user_move.to_i
+     @jon = Math.sqrt(@kevin).round(2)
+
+     render ("square_root.html.erb")
+  end
+
+  def random
+    @user_move = params["move"]
+    @user_integer = params["integer"]
+    @alessandra = @user_move.to_i
+    @michelle = @user_integer.to_i
+    @ben = rand(@alessandra..@michelle)
+
+    render("random.html.erb")
+  end
+
+  def payment
+     @user_move = params["move"]
+     @user_integer = params["integer"]
+     @user_text = params["text"]
+     @leah = (@user_move.to_i)/100.round(2)
+     @russell = (@user_integer.to_i)
+     @danya = @user_text.to_i
+
+# leah is apr
+# russell is years
+# danya is princiapl
+
+    @P = @danya
+    @r = ((@leah/12)/100)
+    @N = @russell*12
+    @bottom = (1-(1+@r)**(-@N))
+    @dylan =((@r*@P)/(@bottom)).round(1)
+
+
+
+
+    render("payment.html.erb")
+  end
+
+
+# apr, years, princ
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:move", { :controller => "calculations", :action => "square_root" })
+  get("/random/:move/:integer", { :controller => "calculations", :action => "random" })
+   get("//payment/:move/:integer/:text", { :controller => "calculations", :action => "payment" })
+
+     get("//square/:move", { :controller => "calculations", :action => "square" })
+
+
+
 end
+
+
+
+
+
+
